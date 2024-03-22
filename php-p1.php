f0a3981446aa9fd281b05605c18514535bac2bd8
Đỗ Mạnh Quang <45645553+DoManhQuang@users.noreply.github.com>
2024-03-22 20:40:40 +0700

    bug

--- php-p1.php
+++ php-p1.php
@@ -10,9 +10,7 @@ function myArr(){
     ];
       $flag = 100;
       foreach ($data as $key => $value) {
-        if($flag == "100"){
           echo "key: $key ---- value: $value <br>";
-        }
       }
   };
 ?>
